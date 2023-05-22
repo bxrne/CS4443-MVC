485506651a70491d0a4028ec5702e727d0d07401
Adam Byrne <adamrbyrne@gmail.com>
2023-05-22 09:44:57 +0100

    initial setup

---
+++ src/main/java/com/ise/RMIS/RmisApplication.java
@@ -0,0 +1,13 @@
+package com.ise.RMIS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ise/RMIS/RmisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ise.RMIS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RmisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
