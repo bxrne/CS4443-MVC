8d73b4278467c6ff3a9d17e572f4b7253eca3a6d
Adam Byrne <adamrbyrne@gmail.com>
2023-05-29 11:46:28 +0100

    build error

--- src/main/java/com/ise/RMIS/RmisApplication.java
+++ src/main/java/com/ise/RMIS/RmisApplication.java
@@ -2,8 +2,9 @@ package com.ise.RMIS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = WebSocketServletAutoConfiguration.class)
 public class RmisApplication {
 
 	public static void main(String[] args) {
